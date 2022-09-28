30b9d64e38fa679faf001b5bf97a905fc055712e
Obsuliman Leyva <obsuliman@gmail.com>
2022-09-27 23:09:20 -0400

    adding form

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ let footer = document.querySelector("footer");
 
 let copyright = document.createElement("p");
 
-copyright.innerHTML = "Obsuliman " + thisYear;
+copyright.innerHTML = "Obsuliman " + thisYear + ".";
 
 footer.appendChild(copyright);
 
-let skills =["JavaScrip","Html","CSS"];
+let skills =["JavaScript","Html","CSS"];
 
 let skillsSection = document.getElementById("skills");
 
@@ -21,3 +21,44 @@ for(let i = 0; i < skills.length; i++){
     skill.innerText= skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementsByName("leave_message")[0];
+//const removeButton = document.createElement("button");
+
+function submitFunction(event){
+    event.preventDefault();
+   let fullName = event.target.name.value;
+   let emailAddress = event.target.email.value;
+   let enterMessage =event.target.message.value;
+
+    console.log(fullName);
+    console.log(emailAddress);
+    console.log(enterMessage);
+
+    let messageSection = document.getElementById("message");
+    
+    let messageList =messageSection.querySelector("ul");
+
+    let newMessage = document.createElement("li");
+
+    newMessage.innerHTML = '<a href="mailto:' + emailAddress + '">' + fullName +'</a>' + '<span>' + ' Wrote: ' + enterMessage + '</span> ' ;
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+
+    removeButton.setAttribute("type", "button");
+
+    removeButton.addEventListener("click",funcButtonRmv);
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    document.getElementsByName("leave_message")[0].reset();
+}
+
+function funcButtonRmv(event){
+    const removeButton = event.target;
+    let entry = removeButton.parentNode;
+    entry.remove();
+}
+
+messageForm.addEventListener("submit", submitFunction);
+
+
